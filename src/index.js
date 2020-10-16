a4f649106c8e2362199088071d836e047366eab8
Girija123 <girijas12@gmail.com>
2020-10-16 20:29:16 +0100

    search function

--- src/index.js
+++ src/index.js
@@ -1,27 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import About from '../src/About/about'
-import Contact from '../src/Contact/contact'
-import Footer from '../src/Footer/footer'
-import Header from '../src/Header/header'
-import Showcase from '../src/Showcase/showcase.js'
 
 const layout ={
   color: 'blue'
 }
 
-class Home extends React.Component {
-
+/*class Home extends React.Component {
   
-
   render(){
     return(
         <div className="App">
          <Header style ={layout}/>
-         {/*<Showcase />
-         <About />
-         <Contact />
-         <Footer /> */}
+         <Search />
         </div>
     )
   }
@@ -30,4 +20,69 @@ class Home extends React.Component {
 
 ReactDOM.render (
   <Home />, document.getElementById('root')
-)
+) */
+
+
+
+
+class Search extends React.Component  {
+  state = {
+
+      products: ['Jeans','Dresses','TShirts','Shoes'],
+      searchTerm: ''
+  }
+
+  getSearchTerm = (event)=> {
+      this.setState({searchTerm: event.target.value})
+  }
+
+  dynamicSearch = () => {
+
+        if (this.state.searchTerm.length===1)                     
+        return this.state.products.filter(product =>product.toString().toLowerCase().includes(this.state.searchTerm.toLowerCase()))
+        else
+        return [];        
+        // return this.state.products.filter(product => (product.toLowerCase().indexOf()>-1))
+  }
+  
+ 
+  render(){
+      return (
+          <div>
+              <label id  = "searchLbl">Search For a product</label>
+              <input type = "text" id  = 'searchFld'  placeholder='Search' className = '' onChange={this.getSearchTerm}/>
+              <SearchResults searchResults = {this.dynamicSearch()}/>
+                
+              </div>
+      )
+  }
+
+}
+
+
+class SearchResults extends Component {
+
+
+
+  render(){
+    return(
+      <div>
+         {this.props.searchResults.map(result => <Result result = {result} />)}
+      </div>
+    )
+  }
+
+}
+
+class Result extends Component {
+  render()
+  {
+    return(
+    <div>{this.props.result}</div>
+    )
+  }
+}
+
+ReactDOM.render (
+  <Search />, document.getElementById('root')
+) 
