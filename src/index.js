5a4aaaf8b50ffbca7fccf8f2ff36ddee7899b20c
Girija Selvakumar <girija.selvakumar@missguided.com>
2020-10-18 19:01:43 +0100

    react checkbox

--- src/index.js
+++ src/index.js
@@ -1,15 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+let checkBoxOption = ["id", "name", "description", "price", "image", "image-title"]
+
+class CheckBoxDisplay extends React.Component {
+  state = { product: [],checkedbox:false,labelName:'id' }
+  componentDidMount() {
+    fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
+      .then(product => product.json())
+      .then(product => this.setState({ product }))
+  }
+
+  getInput=(event)=>{
+    {this.setState({checkedbox:event.target.checked})}
+    //var labelItemName = document.getElementById('checkboxitem').innerHTML
+    {this.setState({labelName:event.target.name})}
+    
+    
+  }
+
+
+
+  selectedBoxCheck=()=>{
+    if(this.state.checkedbox){
+      return this.state.product
+    }
+    else{
+      return []
+    }
+  }
+
+  render() {
+    let { checkBoxes } = this.props
+    return (
+      <div>
+        {checkBoxes.map(checkboxName =>
+          <div>
+            <input type="checkbox" id='checkinput' name={checkboxName} onChange={this.getInput}/>
+            <label id='checkboxitem' name='options' >{checkboxName}</label>
+          </div>
+          
+
+        )}
+        <DisplayMatchedValue checkedValue={this.selectedBoxCheck()} checkItem={this.state.labelName} />
+
+      </div>
+    )
+  }
+}
+
+class DisplayMatchedValue extends React.Component{
+  
+  render(){
+    let {checkedValue}=this.props
+    let {checkItem} = this.props
+    return(
+      <div>
+      {checkedValue.map((content,i)=>
+       
+        <div>{content[checkItem]}</div>
+      )}
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-
-serviceWorker.unregister();
+  <CheckBoxDisplay checkBoxes={checkBoxOption} />, document.getElementById('root')
+)
