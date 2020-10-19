f99e9a1cca2a7a1320c673cef85da5c47ed9ff68
Girija Selvakumar <girija.selvakumar@missguided.com>
2020-10-19 19:31:59 +0100

    colour filter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM, { render } from 'react-dom';
+
+
+
+let colours = ['red', 'blue', 'black', 'pink', 'white']
+
+let colourResult = [{ red: "am red", blue: "am blue", black: "am black", pink: "am pink", white: "am white" }]
+
+class ColourFilter extends React.Component {
+
+  state = { colourPicked: '', chosenColour: '' }
+
+  getAction = (event) => {
+    this.setState({ colourPicked: event.target.text }, () => {
+      colourResult.map((col) => {
+        this.setState({ chosenColour: col[this.state.colourPicked] })
+      })
+    })
+  }
+  render() {
+    let { colourOption } = this.props
+    return (
+      <div>
+        {colourOption.map(colour =>
+          <div>
+            <ul>
+              <li><a href="#" name={colour} onClick={this.getAction}>{colour}</a></li>
+            </ul>
+          </div>
+
+        )}
+        <div>{this.state.chosenColour}</div>
+      </div>
+
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-
-serviceWorker.unregister();
+  <ColourFilter colourOption={colours} />, document.getElementById('root')
+)  
