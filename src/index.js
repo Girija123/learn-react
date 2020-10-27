02412f3328d401449a6491edbbc1c2cdd03e575f
Girija Selvakumar <girija.selvakumar@missguided.com>
2020-10-27 19:53:00 +0000

    inc dec value

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+class TimerDisplay extends React.Component {
+
+  state = { timerValue: '' }
+
+  getNumber = (event) => {
+    { this.setState({ timerValue: event.target.value }) }
+  }
+
+  increaseInput=()=>{
+     
+     var addNumber = document.getElementById('displayTimer').innerHTML;
+     addNumber = Number.parseInt(addNumber)+1;
+     this.setState({timerValue:addNumber})
+  }
+
+  decreaseInput=()=>{
+    var decreaseNumber = document.getElementById('displayTimer').innerHTML;
+    decreaseNumber=decreaseNumber-1;
+    this.setState({timerValue:decreaseNumber})
+  }
+  
+  render() {
+    return (
+      <div>
+        <label>Enter Value</label>
+        <input type='number' onChange={this.getNumber} />
+        <button onClick={this.increaseInput}>Add</button>
+        <button onClick={this.decreaseInput}>Sub</button>
+        <label id="displayTimer">{this.state.timerValue}</label>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-
-serviceWorker.unregister();
+  <TimerDisplay />, document.getElementById('root')
+)
