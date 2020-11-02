c5a1970207f18bd2bed61fb7fb55c2b640090744
Girija Selvakumar <girija.selvakumar@missguided.com>
2020-11-02 22:04:42 +0000

    login

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+let users = { username: 'girijas123', password: 'Maturity123@' }
+
+class Login extends React.Component {
+  
+  state = { username: '', password: '' }
+  
+  username = (event) => {
+    this.setState({ username: event.target.value })
+    console.log(this.state.username);
+  }
+
+  password = (event) => {
+    this.setState({ password: event.target.value })
+  }
+
+  loginValidation = () => {
+    if (this.state.username === this.props.users.username && this.state.password === this.props.users.password) {
+      document.getElementById('displayText').innerText = "You are successfully logged in"
+    }
+    else {
+      document.getElementById('displayText').innerText = "please verify your username and password"
+    }
+  }
+
+  render() {
+
+    return (
+
+      <div>
+        <h1>Login</h1>
+        <label>Username</label>
+        <input type='text' onChange={this.username} />
+        <label>Password</label>
+        <input type='text' onChange={this.password} />
+        <button onClick={this.loginValidation}>Submit</button>
+        <label id='displayText'></label>
+      </div>
+    )
+  }
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-
-serviceWorker.unregister();
+  <Login users={users} />, document.getElementById('root')
+)
