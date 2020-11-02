ba28280f511beba4d5b4ae8a431163a2d8f85850
Girija Selvakumar <girija.selvakumar@missguided.com>
2020-11-02 20:07:04 +0000

    new year timer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+
+class NewYearTimer extends React.Component {
+
+  componentDidMount() {
+    var time = setInterval(function () {
+      var date = Date.now()
+      var newYearDate = new Date('Jan 01, 2021 00:00:00').getTime();
+      var daysToGo = newYearDate - date
+      var days = Math.floor(daysToGo / (1000 * 60 * 60 * 24))
+      var hours = Math.floor((daysToGo % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      var minutes = Math.floor((daysToGo % (1000 * 60 * 60)) / (1000 * 60))
+      var seconds = Math.floor((daysToGo % (1000 * 60)) / 1000)
+      document.getElementById('countDownDate').innerHTML = +days + "days" + " " + +hours + "hours" + " " + +minutes + "minutes" + " " + +seconds + "seconds"
+      if (daysToGo < 0) {
+        clearInterval(time);
+        document.getElementById('countDownDate').innerHTML = "Happy New year, Welcome 2021"
+      }
+
+    }, 1000)
+
+  }
+
+  render() {
+    return (
+      <div>
+        <label>Days To Go For new Year</label>
+        <p id="countDownDate"></p>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-
-serviceWorker.unregister();
+  < NewYearTimer />, document.getElementById('root')
+)
